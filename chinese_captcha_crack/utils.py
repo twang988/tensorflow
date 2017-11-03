c0dbf2ba21c4e9c27833007d0339978595714390
wang5150753 <451170340@qq.com>
2017-11-03 16:18:52 +0800

    Update utils.py

--- chinese_captcha_crack/utils.py
+++ chinese_captcha_crack/utils.py
@@ -5,24 +5,12 @@ from PIL import Image
 
 
 def char2pos(c):
-    """
-    字符验证码，字符串转成位置信息
-    :param c:
-    :return:
-    """
-
     return gen_char_set.index(c)
 
 
 
 
 def pos2char(char_idx):
-    """
-    根据位置信息转化为索引信息
-    :param char_idx:
-    :return:
-    """
-
     return gen_char_set[char_idx]
 
 
@@ -45,9 +33,7 @@ def text2vec(text):
     text_len = len(text)
     if text_len > MAX_CAPTCHA:
         raise ValueError('验证码最长MAX_CAPTCHA个字符')
-
     vector = np.zeros(MAX_CAPTCHA * CHAR_SET_LEN)
-
     for i, c in enumerate(text):
         idx = i * CHAR_SET_LEN + char2pos(c)
         vector[idx] = 1
@@ -59,11 +45,9 @@ def vec2text(vec):
     char_pos = vec.nonzero()[0]
     text = []
     for i, c in enumerate(char_pos):
-        char_at_pos = i  # c/63
+        char_at_pos = i
         char_idx = c % CHAR_SET_LEN
-
         char_code = pos2char(char_idx)
-
         text.append(char_code)
     return "".join(text)
 
