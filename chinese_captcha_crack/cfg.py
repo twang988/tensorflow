474cb96d88c5210ea940a2426b310268d73ebdb4
wang5150753 <451170340@qq.com>
2017-11-03 16:12:44 +0800

    Update cfg.py

--- chinese_captcha_crack/cfg.py
+++ chinese_captcha_crack/cfg.py
@@ -1,6 +1,6 @@
-# 验证码中的字符, 就不用汉字了
+
 from os.path import join
-f=open('./chinese.txt','r')
+f=open('./chinese.txt','r')#加载3500个常用汉字
 CH_CHAR=[]
 lines=f.read()
 f.close()
@@ -21,15 +21,15 @@ IMAGE_HEIGHT = 36
 IMAGE_WIDTH = 90
 
 MAX_CAPTCHA = 6  # 验证码最大长度
-print("验证码文本最长字符数", MAX_CAPTCHA)  # 验证码最长4字符; 我全部固定为4,可以不固定. 如果验证码长度小于4，用'_'补齐
+print("验证码文本最长字符数", MAX_CAPTCHA)  # 验证码最长6字符; 我全部固定为6,可以不固定. 如果验证码长度小于6，用'_'补齐
 
 
 CHAR_SET_LEN = len(gen_char_set)
 
 print('CHAR_SET_LEN:', CHAR_SET_LEN)
 
-home_root = './'  # 在不同操作系统下面Home目录不一样
-workspace = join(home_root, 'capt-python-36')  # 用于工作的训练数据集
+home_root = './'  
+
 model_path = join(home_root, 'model')
 model_tag = 'crack_capcha.model'
 save_model = join(model_path, model_tag)
